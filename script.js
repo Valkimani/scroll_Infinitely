2a4d0c88be6d98bcfda95e7c859abc3db167e0a8
Valkimani <66477953+Valkimani@users.noreply.github.com>
2023-06-26 15:52:03 -0400

    Created setAttributes for specific elements in the object.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let photosArray = [];
 // Unsplash API
 // Create a URL constant
 const count = 10;
-const apiKey = 'API_KEY_HERE';
+const apiKey = 'API KEY HERE';
 const topics = 'topics'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&topics=${topics}`;
 
@@ -16,6 +16,16 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 function showPhotos(){
     // Run function for each object in photoArray
     photosArray.forEach((photo) => {
+// <a> element link to unsplash
+const item = document.createElement('a');
+item.setAttribute('href', photo.links.html);
+item.setAttribute('target', '_blank');
+// Create <img> for photo
+const img = document.createElement('img');
+img.setAttribute('src', photo.urls.regular);
+img.setAttribute('alt', photo.alt_description)
+img.setAttribute('title',photo.alt_description)
+img.setAttribute('location',photo.topic_submissions)
 
     });
 }
