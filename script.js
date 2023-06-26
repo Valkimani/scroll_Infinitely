a4c262bf58d7e2cb84397290ffd405f0000997f4
Valkimani <66477953+Valkimani@users.noreply.github.com>
2023-06-26 14:51:08 -0400

    Created another constant and adjusted the apiUrl. Tested the function to confirm functionality in the dev tools.

--- script.js
+++ script.js
@@ -2,5 +2,20 @@
 
 // Create a URL constant
 const count = 10;
-const apiKey = '';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}=${count}`;
+const apiKey = 'API-KEY_HERE';
+const topics = 'topics'
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&topics=${topics}`;
+
+// Get photos from unsplash API
+async function getPhotos(){
+    try{
+const response = await fetch(apiUrl);
+const data = await response.json();
+console.log(data)
+    }catch(error){
+        // catch error here
+    }
+}
+
+// On load 
+getPhotos();
